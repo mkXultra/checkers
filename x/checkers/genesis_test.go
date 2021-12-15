4783fceb6c106106260c4747dda62059d87fdebc
Developer Experience team at Tendermint <hello@tendermint.com>
2021-12-15 18:27:44 +0900

    Initialized with Starport

---
+++ x/checkers/genesis_test.go
@@ -0,0 +1,23 @@
+package checkers_test
+
+import (
+	"testing"
+
+	keepertest "github.com/mkXultra/checkers/testutil/keeper"
+	"github.com/mkXultra/checkers/x/checkers"
+	"github.com/mkXultra/checkers/x/checkers/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, genesisState)
+	got := checkers.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
---
+++ x/checkers/keeper/grpc_query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"github.com/mkXultra/checkers/x/checkers/types"
+)
+
+var _ types.QueryServer = Keeper{}
