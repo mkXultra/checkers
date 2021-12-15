9b8b3729fe440165a86570d0c5c66d0045f3ee36
mkXultra <eeapprenctice@gmail.com>
2021-12-15 19:51:54 +0900

    implement error

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -9,4 +9,8 @@ import (
 // x/checkers module sentinel errors
 var (
 	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrGameNotFound     = sdkerrors.Register(ModuleName, 1104, "game by id not found: %s")
+	ErrCreatorNotPlayer = sdkerrors.Register(ModuleName, 1105, "message creator is not a player: %s")
+	ErrNotPlayerTurn    = sdkerrors.Register(ModuleName, 1106, "player tried to play out of turn: %s")
+	ErrWrongMove        = sdkerrors.Register(ModuleName, 1107, "wrong move")
 )
