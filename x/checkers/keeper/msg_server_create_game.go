6f1b7335c9a42154d5f890e0248db826c79132ed
mkXultra <eeapprenctice@gmail.com>
2021-12-15 19:46:53 +0900

    implement create game message handling

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -5,13 +5,37 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/mkXultra/checkers/x/checkers/types"
+	rules "github.com/mkXultra/checkers/x/checkers/rules"
 )
 
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+			panic("NextGame not found")
+	}
+	newIndex := strconv.FormatUint(nextGame.IdValue, 10)
 
-	return &types.MsgCreateGameResponse{}, nil
+	storedGame := types.StoredGame{
+			Creator: msg.Creator,
+			Index:   newIndex,
+			Game:    rules.New().String(),
+			Red:     msg.Red,
+			Black:   msg.Black,
+	}
+
+	err := storedGame.Validate()
+	if err != nil {
+			return nil, err
+	}
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	nextGame.IdValue++
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	return &types.MsgCreateGameResponse{
+			IdValue: newIndex,
+	}, nil
 }
