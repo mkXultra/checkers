5370be3069b41bc3c67fa5f03677547040256d3d
mkXultra <eeapprenctice@gmail.com>
2021-12-16 21:45:09 +0900

    implement message move function

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -9,6 +9,8 @@ import (
 // x/checkers module sentinel errors
 var (
 	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrInvalidCreator   = sdkerrors.Register(ModuleName, 1100, "creator address is invalid: %s")
+	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1103, "game cannot be parsed")
 	ErrGameNotFound     = sdkerrors.Register(ModuleName, 1104, "game by id not found: %s")
 	ErrCreatorNotPlayer = sdkerrors.Register(ModuleName, 1105, "message creator is not a player: %s")
 	ErrNotPlayerTurn    = sdkerrors.Register(ModuleName, 1106, "player tried to play out of turn: %s")
